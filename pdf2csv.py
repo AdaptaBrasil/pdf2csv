5b9564371643dfa14b4fa68b302c93d13634486c
Mauro Assis <assismauro@hotmail.com>
2024-08-19 17:33:34 -0300

    Initial commit

---
+++ pdf2csv.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import tabula
+from glob import glob
+#"D:\temp\pdf\lista_A_agosto.pdf"
+def process_command_line():
+    parser = ArgumentParser(description="Importar dados BDF.")
+    parser.add_argument("--input_folder", type=str, required=True,  help="Caminho para a pasta de entrada.")
+    return parser.parse_args()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    args=process_command_line()
+    fnames = glob(f'{args.input_folder}\*.pdf')
+    for fname in fnames:
+        dfl = tabula.read_pdf(fname, pages='all', multiple_tables=False, lattice=True)
+        dfl[0].to_csv(fname.replace('.pdf','.csv'))
+        print(f'File {fname} converted.')
+
+
